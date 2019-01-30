b6de8ff0b95b704cc3d88337421734f9cc55b071
Geisa Nepomuceno <Geisa.Nepomuceno@ibm.com>
2019-01-30 07:54:10 -0200

    Initialized project

---
+++ lala.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Lala extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            uhu: true
+        }
+    }
+
+    OnUpdate() {
+        
+    }
+}
